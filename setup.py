347cd561134074ddb8ed8e050b460385f5c41354
caotrido <ctdo@02DOCAOTT14.Mazars.fr>
2023-08-25 16:58:12 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An example of an end to end project in machine learning using the air quality dataset from UCI',
+    author='caotrido',
+    license='MIT',
+)
